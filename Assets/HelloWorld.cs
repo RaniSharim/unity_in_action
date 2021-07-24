678a7af8b0793d7a42624d70f167c506185ebd55
rsharim <rani.sharim@gmail.com>
2021-07-24 10:15:37 +0300

    change gitignore

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
-{
+public class HelloWorld : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         Debug.Log("Hello world");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
